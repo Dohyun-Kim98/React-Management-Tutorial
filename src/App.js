59f21fffc5ff4d2633f6ac4109acde8878493518
Dohyun-Kim98 <ehgusrlaeh@naver.com>
2020-11-25 11:28:07 +0900

    Create React Project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import logo from './logo.svg';
+import React from 'react'
+import Customer from './components/Customer';
 import './App.css';
+
+const customer = {
+  'name':'나동빈',
+  'birthday':'961222',
+  'gender':'남자',
+  'job':'대학생'
+};
 
 function App() {
   return (
-    <div className="gray-background">
-      <img src={logo} lat="logo"/>
-      <h2>Let's developer management system!!!</h2>
-    </div>
+   
+    <Customer 
+    name={customer.name}
+    birthday={customer.birthday}
+    gender={customer.gender}
+    job={customer.job}
+    />
+ 
   );
 }
 
