61acf276a19f48d1a7566285650725c43a7f8f23
Dohyun-Kim98 <ehgusrlaeh@naver.com>
2020-11-25 12:13:59 +0900

    Create React Project

--- src/App.js
+++ src/App.js
@@ -3,16 +3,21 @@ import Customer from './components/Customer';
 import './App.css';
 
 const customer = {
+  'id':1,
+  'image':'https://placeimg.com/64/64/any',
   'name':'나동빈',
   'birthday':'961222',
   'gender':'남자',
   'job':'대학생'
 };
 
+
 function App() {
   return (
    
     <Customer 
+    id={customer.id}
+    image={customer.image}
     name={customer.name}
     birthday={customer.birthday}
     gender={customer.gender}
